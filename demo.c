e3d44653af21b4444df612b03594383d5d0af24a
BlodTor <blodtor@gmail.com>
2024-01-07 01:02:51 +0300

    Условный оператор if - else

--- demo.c
+++ demo.c
@@ -3,7 +3,13 @@ int main(int argc, char * argv []) {
     printf("\tПривет ретро консольщики!\n");
     int j = 0;
     do {
-        printf("Параметр %i = %s\n", j, argv[j]);
+        if (j > 0) { 
+            // если j > 0 значит это параметр переданый программе
+            printf("Параметр %i = %s\n", j, argv[j]);
+        } else if (j == 0) {
+           // еслт j <= 0 то это имя программы
+           printf("Имя программы: %s\n", argv[j]);
+        }
         j++; // j = j + 1;
     } while (j < argc);
     return 0;
