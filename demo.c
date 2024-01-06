f23cef97f7eca68e3e703f6c25232d2387f12e65
BlodTor <blodtor@gmail.com>
2024-01-07 00:51:39 +0300

    Добавлен цикл

--- demo.c
+++ demo.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(int argc, char * argv []) {
     printf("\tПривет ретро консольщики!\n");
+    for (int j = 0; j < argc; j = j + 1 ) {
+	printf("Параметр %i = %s\n", j, argv[j]);
+    }
     return 0;
 }
