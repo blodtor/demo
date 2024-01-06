f3408f981d96930c1555f7f5bd075ca0ae2d28ea
BlodTor <blodtor@gmail.com>
2024-01-07 00:56:22 +0300

    Цикл do - while

--- demo.c
+++ demo.c
@@ -2,9 +2,9 @@
 int main(int argc, char * argv []) {
     printf("\tПривет ретро консольщики!\n");
     int j = 0;
-    while (j < argc) {
+    do {
         printf("Параметр %i = %s\n", j, argv[j]);
         j++; // j = j + 1;
-    }
+    } while (j < argc);
     return 0;
 }
